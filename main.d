42f579712111cdf2ccf29ea1de5c00d0b578f313
Emmankoko <emmankoko519@gmail.com>
2024-09-16 20:54:59 +0100

    improve main

--- main.d
+++ main.d
@@ -6,29 +6,71 @@ void writeToCppFile();
 void setInstantiate(File);
 void listInstantiate(File);
 void vectorInstantiate(File);
-void declareStruct();
-void declareClass();
+void declareSetStruct(File);
+void declareSetClass(File);
+void declarelistStruct(File);
+void declarelistClass(File);
+void declareVectorClass(File);
+void declareVectorStruct(File);
+void declareStruct(File file, string[]);
+void declareClass(File, string[]);
 
-int vectorflag = 0;
+/* storing classes */
+string[] vectorclass;
+string[] listclass;
+string[] setclass;
+
+/* storing structs */
+string[] vectorstruct;
+string[] liststruct;
+string[] setstruct;
+
+string[] classes;
+string[] structs;
+
+int vclass = 0;
+int vstruct = 0;
+int lclass = 0;
+int lstruct = 0;
+int sclass = 0;
+int sstruct = 0;
 int listflag = 0;
 int setflag = 0;
-int _class = 0;
-int _struct = 0;
-string class_name;
-string struct_name;
+int vectorflag = 0;
 
 void main(string[] args) {
     getopt(args, std.getopt.config.caseSensitive,
-            "vflag|v", &vectorflag,
-            "lflag|l", &listflag,
-            "sflag|s", &setflag,
-            "cl|C", &class_name,
-            "str|S", &struct_name);
+            "vclass|vc", &vectorclass,
+            "lclass|lc", &listclass,
+            "sclass|sc", &setclass,
+            "vstruct|vs", &vectorstruct,
+            "lstruct|ls", &liststruct,
+            "sstruct|ss", &setstruct);
 
-    if (class_name)
-        _class = 1;
-    if (struct_name)
-        _struct = 1;
+    if (vectorclass.length || vectorstruct.length)
+        vectorflag = 1;
+    if (listclass.length || liststruct.length)
+        listflag = 1;
+    if (setclass.length || setstruct.length)
+        setflag = 1;
+
+    if (vectorclass.length)
+        vclass = 1;
+
+     if (vectorstruct.length)
+        vstruct = 1;
+
+    if (listclass.length)
+        lclass = 1;
+
+    if (liststruct.length)
+        lstruct = 1;
+
+    if (setclass.length)
+        sclass = 1;
+
+    if (setstruct.length)
+        sstruct = 1;
 
     writeToCppFile();
     executeShell("clang++ -c test.cpp");
@@ -39,6 +81,7 @@ void writeToCppFile()
 {
     File cppfile;
     cppfile.open("test.cpp", "a");
+
     if (vectorflag)
         vectorInstantiate(cppfile);
     if (listflag)
@@ -50,47 +93,104 @@ void writeToCppFile()
 
 void vectorInstantiate(File file)
 {
-    file.writeln("\n");
-    if (_class)
-        declareClass(file);
-        file.writeln("template class std::vector<", class_name, ">;\n");
-    if (_struct)
-        declareStruct(file);
-        file.writeln("template struct std::vector<", struct_name, ">;\n");
+    if (vclass)
+        declareVectorClass(file);
+        foreach(name; vectorclass)
+        {
+            file.writeln("template class std::vector<", name, ">;\n");
+        }
+
+    if (vstruct)
+        declareVectorStruct(file);
+        foreach(name; vectorstruct)
+        {
+            file.writeln("template struct std::vector<",name , ">;\n");
+        }
+
     return;
 }
 
 void listInstantiate(File file)
 {
-    file.writeln("\n");
-    if (_class)
-        declareClass(file);
-        file.writeln("template class std::list<", class_name, ">;\n");
-    if (_struct)
-        declareStruct(file);
-        file.writeln("template struct std::list<", struct_name, ">;\n");
+    if (lclass)
+        declarelistClass(file);
+        foreach(name; listclass)
+        {
+            file.writeln("template class std::list<", name, ">;\n");
+        }
+
+    if (lstruct)
+        declarelistStruct(file);
+        foreach(name; liststruct)
+        {
+            file.writeln("template struct std::list<", name, ">;\n");
+        }
+
     return;
 }
 
 void setInstantiate(File file)
 {
-    file.writeln("\n");
-    if (_class)
-        declareClass(file);
-        file.writeln("template class std::set<", class_name, ">;\n");
-    if (_struct)
-        declareStruct(file);
-        file.writeln("template struct std::set<", struct_name, ">;\n");
+    if (sclass)
+        declareSetClass(file);
+        foreach(name; setclass)
+        {
+            file.writeln("template class std::set<", name, ">;\n");
+        }
+
+    if (sstruct)
+        declareSetStruct(file);
+        foreach(name; setstruct)
+        {
+            file.writeln("template struct std::set<", name, ">;\n");
+        }
 
     return;
 }
 
-void declareClass(File file)
+void declareVectorClass(File file)
 {
-    file.writeln("class ", class_name, ";\n");
+    declareClass(file, vectorclass);
 }
 
-void declareStruct(File file)
+void declareVectorStruct(File file)
 {
-    file.writeln("struct ", struct_name, ";\n");
+    declareStruct(file, vectorstruct);
 }
+
+void declarelistClass(File file)
+{
+    declareClass(file, listclass);
+}
+
+void declarelistStruct(File file)
+{
+    declareStruct(file, listclass);
+}
+
+void declareSetClass(File file)
+{
+    declareClass(file, setclass);
+}
+
+void declareSetStruct(File file)
+{
+    declareStruct(file, setstruct);
+}
+
+void declareClass(File file, string[] _class)
+{
+    foreach(name; _class)
+    {
+        file.writeln("class ", name, ";\n");
+    }
+}
+
+void declareStruct(File file, string[] _struct)
+{
+    foreach(name; _struct)
+    {
+        file.writeln("struct ", name, ";\n");
+    }
+}
+
